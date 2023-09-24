380143316acf6472b2977f160e45aa7f2ebacdc4
jhor <jakub.horyd@crealogix.com>
2023-09-24 18:11:24 +0200

    Refactor DriverFactory to use browser from config file, create PropertiesLoader to handle properties, add default properties.

--- src/test/java/com/jakubhoryd/utils/DriverFactory.java
+++ src/test/java/com/jakubhoryd/utils/DriverFactory.java
@@ -2,11 +2,24 @@ package com.jakubhoryd.utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.IOException;
 
 public class DriverFactory {
 
 
-    public static WebDriver getDriver() {
-        return new ChromeDriver();
+    public static WebDriver getDriver() throws IOException {
+        String browserName = PropertiesLoader.loadProperty("browser.name");
+
+        switch (browserName) {
+            case "chrome" -> {
+                return new ChromeDriver();
+            }
+            case "firefox" -> {
+                return new FirefoxDriver();
+            }
+        }
+        return null;
     }
 }
