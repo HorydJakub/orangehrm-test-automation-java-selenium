c3fff6e29ff896a52c9a18cedae7cb1f615264a2
jhor <jakub.horyd@crealogix.com>
2023-09-24 18:56:53 +0200

    Refactor to use Explicit Waits

--- src/test/java/com/jakubhoryd/pages/LoginPage.java
+++ src/test/java/com/jakubhoryd/pages/LoginPage.java
@@ -1,31 +1,39 @@
 package com.jakubhoryd.pages;
 
+import com.jakubhoryd.utils.PropertiesLoader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
 
 public class LoginPage {
 
-    private WebDriver driver;
-    @FindBy(css ="input[name='username']")
+    private final WebDriverWait wait;
+    @FindBy(css = "input[name='username']")
     private WebElement usernameInput;
 
-    @FindBy(css ="input[name='password']")
+    @FindBy(css = "input[name='password']")
     private WebElement passwordInput;
 
-    @FindBy(css ="button.orangehrm-login-button")
+    @FindBy(css = "button.orangehrm-login-button")
     private WebElement loginBtn;
 
-    public LoginPage(WebDriver driver) {
+    public LoginPage(WebDriver driver) throws IOException {
         PageFactory.initElements(driver, this);
-        this.driver = driver;
+        int timeToWaitForElement = Integer.parseInt(PropertiesLoader.loadProperty("wait.timeToWaitForElement"));
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(timeToWaitForElement));
     }
 
     public void login(String username, String password) {
-        usernameInput.sendKeys(username);
-        passwordInput.sendKeys(password);
-        loginBtn.click();
+        wait.until(elementToBeClickable(usernameInput)).sendKeys(username);
+        wait.until(elementToBeClickable(passwordInput)).sendKeys(password);
+        wait.until(elementToBeClickable(loginBtn)).click();
     }
 
 }
