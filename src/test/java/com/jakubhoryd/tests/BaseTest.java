1f466a29769a19d4d8bda31b24b8ba66c88e1d60
jhor <jakub.horyd@crealogix.com>
2023-09-24 18:11:24 +0200

    Refactor DriverFactory to use browser from config file, create PropertiesLoader to handle properties, add default properties.

--- src/test/java/com/jakubhoryd/tests/BaseTest.java
+++ src/test/java/com/jakubhoryd/tests/BaseTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.time.Duration;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
@@ -13,7 +13,7 @@ public class BaseTest {
     protected WebDriver driver;
 
     @BeforeMethod
-    public void setup() {
+    public void setup() throws IOException {
         driver = DriverFactory.getDriver();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
---
+++ src/test/java/com/jakubhoryd/utils/PropertiesLoader.java
@@ -0,0 +1,18 @@
+package com.jakubhoryd.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertiesLoader {
+
+    public static String loadProperty(String propertyName) throws IOException {
+
+        InputStream inputStream = new FileInputStream("src/test/resources/openhrm.properties");
+        Properties properties = new Properties();
+        properties.load(inputStream);
+
+        return properties.getProperty(propertyName);
+    }
+}
