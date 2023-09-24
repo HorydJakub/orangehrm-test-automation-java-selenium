fd2c5ef8568de2473bc823a2b8542b5ffe253bf0
jhor <jakub.horyd@crealogix.com>
2023-09-24 18:56:53 +0200

    Refactor to use Explicit Waits

--- src/test/java/com/jakubhoryd/tests/LoginPageTest.java
+++ src/test/java/com/jakubhoryd/tests/LoginPageTest.java
@@ -4,14 +4,18 @@ import com.jakubhoryd.pages.LoginPage;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+
 public class LoginPageTest extends BaseTest {
 
 
     private LoginPage loginPage;
+
     @BeforeMethod
-    public void beforeMethod() {
+    public void beforeMethod() throws IOException {
         loginPage = new LoginPage(driver);
     }
+
     @Test(testName = "TC-37", description = "LoginPage: Login with valid data")
     public void loginWithValidDataTest() {
 
