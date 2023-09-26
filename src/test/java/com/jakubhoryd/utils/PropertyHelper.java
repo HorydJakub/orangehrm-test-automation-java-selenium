1c0d9b001dbe69d7d2fd09b3a3c17b39f19403de
jhor <jakub.horyd@crealogix.com>
2023-09-27 00:44:54 +0200

    Added screenshot capture for failed tests.

--- src/test/java/com/jakubhoryd/utils/PropertyHelper.java
+++ src/test/java/com/jakubhoryd/utils/PropertyHelper.java
@@ -7,4 +7,12 @@ public class PropertyHelper {
     public static int getWaitingTimeForElements() throws IOException {
         return Integer.parseInt(PropertiesLoader.loadProperty("wait.timeToWaitForElement"));
     }
+
+    public static String getFailedTestsScreenshotDirectory() throws IOException {
+        return PropertiesLoader.loadProperty("path.failedTestsScreenshotDirectory");
+    }
+
+    public static String getPreferedScreenshotExtension() throws IOException {
+        return PropertiesLoader.loadProperty("screenshot.preferedScreenshotExtension");
+    }
 }
