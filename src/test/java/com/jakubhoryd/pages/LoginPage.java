0e19fbc4c213c37d80133eb075fe9fd31ce10a82
jhor <jakub.horyd@crealogix.com>
2023-10-09 20:59:24 +0200

    New tests for login functionality

--- src/test/java/com/jakubhoryd/pages/LoginPage.java
+++ src/test/java/com/jakubhoryd/pages/LoginPage.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.IOException;
 import java.time.Duration;
+import java.util.List;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
 
@@ -16,16 +17,46 @@ public class LoginPage {
 
     private final WebDriverWait wait;
     @FindBy(css = "input[name='username']")
-    private WebElement usernameInput;
+    public WebElement usernameInput;
 
     @FindBy(css = "input[name='password']")
-    private WebElement passwordInput;
+    public WebElement passwordInput;
 
     @FindBy(css = "button.orangehrm-login-button")
-    private WebElement loginBtn;
+    public WebElement loginBtn;
 
     @FindBy(css = "div.orangehrm-login-error p.oxd-alert-content-text")
     public WebElement invalidCredentialsWarning;
+
+    @FindBy(css = "span.oxd-input-field-error-message")
+    public List<WebElement> requiredFieldErrorMessages;
+
+    @FindBy(css = "div.orangehrm-login-branding img")
+    public WebElement orangeHrmLogo;
+
+    @FindBy(css = "p.orangehrm-login-forgot-header")
+    public WebElement forgotPasswordOption;
+
+    @FindBy(css = "p.orangehrm-login-forgot-header")
+    public List<WebElement> socialMediaBtns;
+
+    @FindBy(css = "div.orangehrm-copyright-wrapper p:nth-child(1)")
+    public WebElement orangeHrmVersionInfo;
+
+    @FindBy(css = "div.orangehrm-copyright-wrapper p:nth-child(2)")
+    public WebElement orangeHrmCopyrightInfo;
+
+    @FindBy(xpath = "//a[contains(@href, 'www.linkedin.com')]")
+    public WebElement linkedinBtn;
+
+    @FindBy(xpath = "//a[contains(@href, 'www.facebook.com')]")
+    public WebElement facebookBtn;
+
+    @FindBy(xpath = "//a[contains(@href, 'www.twitter.com')]")
+    public WebElement twitterBtn;
+
+    @FindBy(xpath = "//a[contains(@href, 'www.youtube.com')]")
+    public WebElement youtubeBtn;
 
     public LoginPage(WebDriver driver) throws IOException {
         PageFactory.initElements(driver, this);
