a22800e7cf01a8ba9a0ef42b566ee91bae528a67
Jakub Horyd <j.horyd@outlook.com>
2024-06-02 14:41:27 +0200

    Implement cucumber test as an example

---
+++ src/test/java/com/jakubhoryd/core/cucumber/runners/CucumberTestRunner.java
@@ -0,0 +1,12 @@
+package com.jakubhoryd.core.cucumber.runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com.example.stepdefinitions",
+        plugin = {"pretty", "html:target/cucumber-reports.html"}
+)
+public class CucumberTestRunner extends AbstractTestNGCucumberTests {
+}
